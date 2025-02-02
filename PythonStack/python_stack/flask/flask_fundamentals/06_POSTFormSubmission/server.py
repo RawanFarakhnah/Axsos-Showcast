c2d2256bb3c673228d899136bf210a37c3e85747
rawan-farakhna <rawanali1997@yahoo.com>
2025-02-02 23:43:41 +0200

    update redirection and sessions

--- PythonStack/python_stack/flask/flask_fundamentals/06_POSTFormSubmission/server.py
+++ PythonStack/python_stack/flask/flask_fundamentals/06_POSTFormSubmission/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,9 +7,23 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/submit')
-def form_submit():
-    return "Success"
+@app.route('/get-register')
+def show_get_form():
+    return render_template("form_with_get.html")
+
+@app.route('/add_with_get', methods=['GET'])
+def add_with_get_form():
+   data = request.args # Use `request.args` for GET requests
+   return render_template("success.html",data=data)
+
+@app.route('/post-register')
+def show_post_form():
+    return render_template("form_with_post.html")
+
+@app.route('/add_with_post', methods=['POST'])
+def add_with_post_form():
+   data = request.form
+   return render_template("success.html",data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
